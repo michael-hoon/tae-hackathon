cd44086a1e21f86cf199a9d7a0550b4cb265fc25
nathanansel28 <nathanansel.edu@gmail.com>
2024-07-26 02:04:22 +0800

    chore: init

---
+++ eda/eda_nathan.R
@@ -0,0 +1,7 @@
+setwd("C:\\SUTD\\40.016 TAE - The Analytics Edge\\000 - COMPETITION")
+df_train <- read.csv("data\\train2024.csv")
+df_test <- read.csv("data\\test2024.csv")
+
+# SUMMARY
+str(df_train)
+summary(df_train)
