b6f7b1578f9a9bc9ad3a328c46a0da203bcdc88f
nathanansel28 <nathanansel.edu@gmail.com>
2024-07-31 00:47:27 +0800

    fix: add initial debugging steps

--- models/multinomial_logit_model_2.R
+++ models/multinomial_logit_model_2.R
@@ -22,20 +22,20 @@ df_test <- subset(test_raw, select = -c(segmentind,year,miles,milesind,night,nig
 # 1. Converting the car features into factors / categorical variables
 car_features <- c('CC1','GN1','NS1','BU1','FA1','LD1','BZ1','FC1','FP1','RP1','PP1','KA1','SC1','TS1','NV1','MA1','LB1','AF1','HU1',
                   'CC2','GN2','NS2','BU2','FA2','LD2','BZ2','FC2','FP2','RP2','PP2','KA2','SC2','TS2','NV2','MA2','LB2','AF2','HU2',
-                  'CC3','GN3','NS3','BU3','FA3','LD3','BZ3','FC3','FP3','RP3','PP3','KA3','SC3','TS3','NV3','MA3','LB3','AF3','HU3',
-                  'CC4','GN4','NS4','BU4','FA4','LD4','BZ4','FC4','FP4','RP4','PP4','KA4','SC4','TS4','NV4','MA4','LB4','AF4','HU4')
+                  'CC3','GN3','NS3','BU3','FA3','LD3','BZ3','FC3','FP3','RP3','PP3','KA3','SC3','TS3','NV3','MA3','LB3','AF3','HU3')
+                  # 'CC4','GN4','NS4','BU4','FA4','LD4','BZ4','FC4','FP4','RP4','PP4','KA4','SC4','TS4','NV4','MA4','LB4','AF4','HU4')
 for (car_feature in car_features){
   df_train[, car_feature] <- factor(df_train[, car_feature])
   df_test[, car_feature] <- factor(df_test[, car_feature])
 }
 
-# 2. Check and remove factors with only one level
-for (car_feature in car_features) {
-  if (length(unique(df_train[, car_feature])) <= 1) {
-    df_train[, car_feature] <- NULL
-    df_test[, car_feature] <- NULL
-  }
-}
+# # 2. Check and remove factors with only one level
+# for (car_feature in car_features) {
+#   if (length(unique(df_train[, car_feature])) <= 1) {
+#     df_train[, car_feature] <- NULL
+#     df_test[, car_feature] <- NULL
+#   }
+# }
 
 # 3. Create dummy variables
 dummy <- dummyVars(" ~ .", data = df_train)
@@ -59,9 +59,7 @@ train_features <- subset(train_features, select = -c(Case, No, Task))
 train_features$No <- seq(1, nrow(train_features))
 train <- merge(id_train, train_features, on='No')
 
-
-
-S_train <- dfidx(subset(train), shape="wide", choice="Choice", sep="", varying = c(8:217), idx = c("No", "Case"))
+S_train <- dfidx(subset(train, Task <=12), shape="wide", choice="Choice", sep="", varying = c(8:191), idx = c("No", "Case"))
 M <- mlogit(Choice~CC+GN+NS+BU+FA+LD+BZ+FC+FP+RP+PP+KA+SC+TS+NV+MA+LB+AF+HU+Price-1, data=S_train)
 summary(M)
 M <- mlogit(Choice~CC+GN+BU+FA+LD+BZ+FC+RP+PP+KA+SC+TS+NV+MA+LB+HU+Price-1, data=S_train)
